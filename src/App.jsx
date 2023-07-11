633b61efb72243e32dfcec7c94c71174699360b9
Erathia Xie <Ecytchia@gmail.com>
2023-07-11 14:13:39 +0800

    Add massive data

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
 
       {/* header */}
       <div className="uppercase font-bold  text-2xl text-center mb-3">
-        ChatGpt2.0
+        E. Eugene Xie's  Resume
       </div>
 
       {/* body */}
--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
-export const fetchResponse =  async(chat) => {
+export const fetchResponse = async (chat) => {
     try {
         // after depoloyment you should change the fetch URL below
-        const response = await fetch('http://localhost:3080', { 
+        const response = await fetch('http://localhost:3080', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                message: chat.map((message)=> message.message).join(" \n ")
+                message: chat.map((message) => message.message).join(" \n ")
             })
         })
 
