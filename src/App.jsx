c58f05337cb9e4b09e8898fc7e3a70bd52f77083
Erathia Xie <Ecytchia@gmail.com>
2023-07-12 08:24:18 +0800

    Changed title

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
 
       {/* header */}
       <div className="uppercase font-bold  text-2xl text-center mb-3">
-        E. Eugene Xie's  Resume
+        Eugene AI doppelgänger
       </div>
 
       {/* body */}
